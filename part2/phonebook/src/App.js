086fef6fe77a7d6bf08baee402e282a9212130d4
ferueda <fe.rueda@outlook.com>
2020-03-27 10:21:14 -0300

    finished exercises 2.7, 2.8 and 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,40 +12,92 @@ const Heading = ({ type, text }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: 'Felipe', phone: '234234324', id: 1 },
+    { name: 'Mayra', phone: '93564324', id: 2 }
+  ]);
   const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
+  const [newSearch, setNewSearch] = useState('');
+  const [showAll, setShowAll] = useState(true);
 
   const handleNameChange = e => {
     setNewName(e.target.value);
   };
 
+  const handlePhoneChange = e => {
+    setNewPhone(e.target.value);
+  };
+
+  const handleSearchChange = e => {
+    setNewSearch(e.target.value);
+    setShowAll(false);
+  };
+
   const addPerson = e => {
     e.preventDefault();
+
+    if (persons.find(person => person.name.includes(newName))) {
+      alert(`${newName} is already added to the phonebook`);
+      return;
+    }
+
     if (newName !== '') {
       const personObject = {
         name: newName,
-        id: persons.length + 1
+        id: persons.length + 1,
+        phone: newPhone
       };
       setPersons(persons.concat(personObject));
       setNewName('');
     }
   };
 
+  const displayBlock = {
+    display: 'block'
+  };
+
+  const personsToShow = showAll
+    ? persons
+    : persons.filter(person =>
+        person.name.toLowerCase().includes(newSearch.toLowerCase())
+      );
+
   return (
     <div>
       <Heading type='h2' text='Phonebook' />
+      <label htmlFor='search'>Filter shown with: </label>
+      <input
+        type='text'
+        name='search'
+        value={newSearch}
+        onChange={handleSearchChange}
+      />
+      <Heading type='h2' text='Add new' />
       <form onSubmit={addPerson}>
         <input
+          type='text'
           value={newName}
           onChange={handleNameChange}
           placeholder='Enter name...'
         />
-        <button type='submit'>Add</button>
+        <input
+          type='text'
+          value={newPhone}
+          onChange={handlePhoneChange}
+          style={displayBlock}
+          placeholder='Enter phone number'
+        />
+        <button style={displayBlock} type='submit'>
+          Add
+        </button>
       </form>
       <Heading type='h2' text='Numbers' />
       <div>
-        {persons.map(person => (
-          <p key={person.id}>{person.name}</p>
+        {personsToShow.map(person => (
+          <p key={person.id}>
+            {person.name} - {person.phone}{' '}
+          </p>
         ))}
       </div>
     </div>
