10bede618771f35a828d259712e742a2b8ec802b
ferueda <fe.rueda@outlook.com>
2020-03-31 20:25:12 -0300

    finished exercise 3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const persons = [
+let persons = [
   {
     name: 'Arto Hellas',
     number: '040-123456',
@@ -48,6 +48,12 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  persons = persons.filter(person => person.id !== id);
 });
 
 const PORT = 3001;
