98dc3f0cec12e18620b3ba9ea39c380ec70ece45
ferueda <fe.rueda@outlook.com>
2020-04-04 14:52:45 -0300

    restructured the app following part 4

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,96 +1,10 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
-const Note = require('./models/note');
+const app = require('./app');
+const http = require('http');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-morgan.token('body', (req) => JSON.stringify(req.body));
+const server = http.createServer(app);
 
-app.use(express.static('build'));
-app.use(express.json());
-app.use(cors());
-app.use(morgan(':method :url :status :response-time ms :body'));
-
-app.get('/api/notes', (req, res) => {
-  Note.find({}).then((notes) => {
-    res.json(notes.map((note) => note.toJSON()));
-  });
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
-
-app.get('/api/notes/:id', (req, res, next) => {
-  Note.findById(req.params.id)
-    .then((note) => {
-      if (note) {
-        res.json(note.toJSON());
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
-
-app.delete('/api/notes/:id', (req, next) => {
-  Note.findByIdAndDelete(req.params.id)
-    .then((res) => {
-      res.json(res.toJSON());
-    })
-    .catch((error) => next(error));
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const body = req.body;
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(req.params.id, note, { new: true })
-    .then((updatedNote) => {
-      res.json(updatedNote.toJSON());
-    })
-    .catch((error) => next(error));
-});
-
-app.post('/api/notes', (req, res, next) => {
-  let body = req.body;
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  });
-
-  note
-    .save()
-    .then((savedNote) => {
-      res.json(savedNote.toJSON());
-    })
-    .catch((error) => next(error));
-});
-
-const unknownEndpoint = (req, res) => {
-  res.status(404).json({ error: 'unknown endpoint' });
-};
-
-const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
-
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return res.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message });
-  }
-
-  next(error);
-};
-
-app.use(unknownEndpoint);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-app.use(errorHandler);
