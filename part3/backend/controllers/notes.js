959922fff37fe30d19a8fd3f342cba341e465bd7
ferueda <fe.rueda@outlook.com>
2020-04-07 11:54:50 -0400

    finished part 4 section d

--- part3/backend/controllers/notes.js
+++ part3/backend/controllers/notes.js
@@ -1,4 +1,5 @@
 const notesRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Note = require('../models/note');
 const User = require('../models/user');
 
@@ -16,10 +17,27 @@ notesRouter.get('/:id', async (req, res) => {
   }
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+
+  return null;
+};
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body;
+  const token = getTokenFrom(request);
 
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!token || !decodedToken) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
